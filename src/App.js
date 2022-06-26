24bb3e44e3096182fdca5057f1e07556dbba24f7
Nathan Watkins Hoagland <watkins92@gmail.com>
2022-06-26 08:22:46 -0700

    parallax home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import redWaterLotus from './img/redWaterLotus.webp';
+// import Ann from './img/ann.jpg';
+// import annLogo from './img/annLogo.jpg';
+// import newHarmonyHeader from './img/newHarmonyHeader.jpg';
+
+
+// const homeBackground = {
+//   backgroundColor: "#37659D",
+// };
+const parallaxHome = {
+  backgroundImage: `url(${redWaterLotus})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  height: "100vh",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Ann Watkins</h1>
       </header>
+      <div className='jumbotron section' style={parallaxHome}>
+
+      </div>
       <h1>Conscious Anatomy Flier</h1>
     </div>
   );
